f8be983332d57a4c0118fff206cd47ed715f2cc0
eddah73 <eddahsjr@gmail.com>
2024-07-10 13:18:38 +0300

    fetch(),DOM

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-// Add your code here
+// // Add your code here
+// function submitData(){
+//   fetch("http://localhost:3000/users",{
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       name: "eddah",
+//       email: "eddah@gmail.com",
+//     }),
+//   })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+
+// }
+// submitData();
+
+  
+  ///const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject);
+// fetch("http://localhost:3000/dogs", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   });
+
+
+function submitData(name, email) {
+  let formData = {
+      name: name,
+      email: email
+  };
+
+  let configObject = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObject)
+      .then(response => {
+          if (!response.ok) {
+              throw Error(response.statusText);
+          }
+          return response.json();
+      })
+      .then(data => {
+          // Assuming the response returns an object with an 'id' field
+          let id = data.id;
+          document.body.append(id); // Append id to the DOM (adjust this according to your needs)
+      })
+      .catch(error => {
+          document.body.append(error.message); // Append error message to the DOM on failure
+      });
+}
+submitData()
